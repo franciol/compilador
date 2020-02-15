c0fb8ef2bdaa9f6d8da058cb648bf1de9d3beb19
francisco aveiro <fran.aveiro@hotmail.com>
2020-02-15 17:40:52 -0300

    Criação de um programa em python, que calcula strings

---
+++ compilador.py
@@ -0,0 +1,28 @@
+import os,sys
+
+argv = sys.argv[1]
+
+operators = []
+
+for i in range(len(argv)):
+    
+    if(argv[i] == "+" or argv[i] == "-"):
+
+        operators.append(i)
+
+
+begining = 0
+numbers = []
+for ops in operators:
+    ss = argv[begining:ops]
+    numbers.append(int(ss))
+    begining = ops+1
+numbers.append(int(argv[begining:]))
+res = numbers[0]
+for ops in range(0,len(operators)):
+    if(argv[operators[ops]] == "+"):
+        res += numbers[ops+1]
+    elif(argv[operators[ops]] == "-"):
+        res -= numbers[ops+1]
+
+print(res)
