df86eb391334d98c3f6dea60053043b3df1a5cf8
Francisco Ciol R. Aveiro <fran.aveiro@hotmail.com>
2020-04-01 11:46:36 -0300

    Terminando implementação AST

--- compilador.py
+++ compilador.py
@@ -1,5 +1,58 @@
 import sys
 import re
+from abc import abstractmethod, ABC
+
+
+class Node(ABC):
+    value = None
+    children = None
+
+    @abstractmethod
+    def Evaluate(self):
+        pass
+
+
+class BinOp(Node):
+    def __init__(self, value):
+        self.children = []
+        self.value = value
+
+    def Evaluate(self):
+        if(self.value == "+"):
+            return self.children[0].Evaluate() + self.children[1].Evaluate()
+        elif(self.value == "-"):
+            return (self.children[0].Evaluate() - self.children[1].Evaluate())
+        elif(self.value == "*"):
+            return self.children[0].Evaluate() * self.children[1].Evaluate()
+        elif(self.value == "/"):
+            return self.children[0].Evaluate() // self.children[1].Evaluate()
+        raise Exception("Error in BinOp: Value unexpected")
+
+
+class UnOp(Node):
+    def __init__(self, value):
+        self.value = value
+
+    def Evaluate(self):
+        if(self.value == "-"):
+            return -self.children.Evaluate()
+        elif(self.value == "+"):
+            return self.children.Evaluate()
+        raise Exception("Error in UnOp: Value unexpected")
+
+
+class IntVal(Node):
+    def __init__(self, value):
+        self.children = None
+        self.value = value
+
+    def Evaluate(self):
+        return self.value
+
+
+class NoOp(Node):
+    def Evaluate(self):
+        pass
 
 
 class Token:
@@ -25,10 +78,8 @@ class Tokenizer:
                 self.actual.value = ""
             elif(self.origin[self.position-1].isdigit()):
                 init = self.position-1
-
                 while (self.origin[init:self.position+1].isdigit() and (self.position+1 <= len(self.origin))):
                     self.position += 1
-
                 end = self.position
                 self.actual.Type = "INT"
                 self.actual.value = int(self.origin[init:end])
@@ -55,7 +106,6 @@ class Tokenizer:
             else:
                 raise Exception(
                     "ERRO", "Operando '%s' não reconhecido na posição %d" % (self.origin[self.position-1], self.position-1))
-            
 
 
 class Parser:
@@ -67,43 +117,61 @@ class Parser:
         Parser.tokens.selectNext()
         try:
             if(Parser.tokens.actual.Type == "INT"):
-                temp = Parser.tokens.actual.value 
+                val = IntVal(Parser.tokens.actual.value)
                 Parser.tokens.selectNext()
-                return temp
+                return val
             elif (Parser.tokens.actual.Type in ["MINUS", "PLUS"]):
-                if(Parser.tokens.actual.Type =="MINUS"):
-                    return -Parser.parseFactor(tokens)
+                if(Parser.tokens.actual.Type == "MINUS"):
+                    un = UnOp("-")
+                    un.children = Parser.parseFactor(tokens)
+                    return un
                 else:
-                    return Parser.parseFactor(tokens)
+                    un = UnOp("+")
+                    un.children = Parser.parseFactor(tokens)
+                    return un
+
+
             elif (Parser.tokens.actual.Type == "OPENPAR"):
                 temp = Parser.parseExpression(tokens)
                 if(Parser.tokens.actual.Type == "CLOSEPAR"):
                     Parser.tokens.selectNext()
                     return temp
-            raise Exception ("ERROR IN FACTOR")
+
+            raise Exception("ERROR IN FACTOR")
         except Exception as e:
             print(e)
-
 
 
     @staticmethod
     def parseTerm(tokens):
         temp_value = Parser.parseFactor(tokens)
-        while(Parser.tokens.actual.Type == "MULT" or Parser.tokens.actual.Type == "DIV"):
-            if(Parser.tokens.actual.Type == "MULT"):
-                temp_value *= Parser.parseFactor(tokens)
-            elif(Parser.tokens.actual.Type == "DIV"):
-                temp_value //= Parser.parseFactor(tokens)
-        return temp_value
+        if(Parser.tokens.actual.Type == "MULT"):
+            term = BinOp("*")
+            term.children.append(temp_value)
+            term.children.append(Parser.parseTerm(tokens))
+
+        elif(Parser.tokens.actual.Type == "DIV"):
+            term = BinOp("/")
+            term.children.append(temp_value)
+            term.children.append(Parser.parseTerm(tokens))
+
+        else:
+            return temp_value
+        return term
 
     @staticmethod
     def parseExpression(tokens):
         temp_value = Parser.parseTerm(tokens)
-        while(Parser.tokens.actual.Type == "PLUS" or Parser.tokens.actual.Type == "MINUS"):
+        if(Parser.tokens.actual.Type not in ["EOF"] and Parser.tokens.actual.Type in ["PLUS", "MINUS"]):
             if(Parser.tokens.actual.Type == "PLUS"):
-                temp_value += Parser.parseTerm(tokens)
+                main = BinOp("+")
+                main.children.append(temp_value)
+
             elif(Parser.tokens.actual.Type == "MINUS"):
-                temp_value -= Parser.parseTerm(tokens)
+                main = BinOp("-")
+                main.children.append(temp_value)
+            main.children.append(Parser.parseExpression(tokens))
+            return main
         return temp_value
 
     @staticmethod
@@ -113,7 +181,8 @@ class Parser:
         if(Parser.tokens.actual.Type == "EOF"):
             return final
         else:
-            raise Exception("Erro", "Input is wrong at position %d (%s) " % (Parser.tokens.position-1,Parser.tokens.origin[Parser.tokens.position-1]))
+            raise Exception("Erro", "Input is wrong at position %d (%s) " % (
+                Parser.tokens.position-1, Parser.tokens.origin[Parser.tokens.position-1]))
 
 
 class PrePro:
@@ -126,5 +195,14 @@ class PrePro:
 
 
 if __name__ == '__main__':
-    value = Parser.run(sys.argv[1])
-    print(value)
+    file_php = (sys.argv[1])
+    if(".php" in file_php):
+        with open(file_php) as fp:
+            lines = fp.readlines()
+            for line in lines:
+                if('\n' in line):
+                    line = line.replace('\n','')
+                value = Parser.run(line)
+                print(value.Evaluate())
+    else:
+        raise Exception("Type error: %s is not a .php file" % (file_php))
